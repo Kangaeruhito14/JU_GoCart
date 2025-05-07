57322f7bfe5ab9e33cdc1eb4e1d238eacd964787
DebashisDhali <debashisdhali104035@gmail.com>
2025-05-07 19:47:56 +0600

    Base Functionalities of Go-Cart

---
+++ JU_GoCart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for JU_GoCart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JU_GoCart.settings')
+
+application = get_asgi_application()
---
+++ gocart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GocartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gocart'
